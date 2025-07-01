8ea160d617dd3bb32fea8789fc150816ca63ff79
azatbek <sadykowazatbek@gmail.com>
2025-07-02 03:47:41 +0600

    feat: changed ProtectedRoute

--- frontend/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactNode } from 'react';
 import { useAppSelector } from '../../app/hooks';
 import { selectUser } from '../../features/users/usersSlice';
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { appRoutes } from '../../utils/constants.ts';
 
 interface Props {
@@ -10,17 +10,15 @@ interface Props {
 
 const ProtectedRoute: FC<Props> = ({ children }) => {
   const user = useAppSelector(selectUser);
-  const location = useLocation();
-  const isInstitutionCreatePath = location.pathname.includes('/institution/create');
 
   const isClient = user?.role === 'client';
   const isEmployee = user?.role !== 'client';
 
   if (!user) {
     return <Navigate to={appRoutes.login} />;
-  } else if (isClient && !isInstitutionCreatePath) {
+  } else if (isClient) {
     return children;
-  } else if (isEmployee && isInstitutionCreatePath) return children;
+  } else if (isEmployee) return children;
   return <Navigate to={appRoutes.home} />;
 };
 
