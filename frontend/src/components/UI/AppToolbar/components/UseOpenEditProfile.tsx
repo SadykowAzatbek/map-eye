c691603ae1922193d4fc10af738cffa2bfb3c2a7
azatbek <sadykowazatbek@gmail.com>
2025-07-12 22:32:21 +0600

    feat: added profile editor menu

---
+++ frontend/src/components/UI/AppToolbar/components/UseOpenEditProfile.tsx
@@ -0,0 +1,11 @@
+import { create } from 'zustand';
+
+type openProfileTypes = {
+  isOpen: boolean;
+  toggleOpen: () => void;
+}
+
+export const useOpenEditProfile = create<openProfileTypes>((set) => ({
+  isOpen: false,
+  toggleOpen: () => set((state) => ({isOpen: !state.isOpen})), // С false в true и наоборот
+}));
