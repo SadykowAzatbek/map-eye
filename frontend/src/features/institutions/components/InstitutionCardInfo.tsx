41ee0f2edd2cd5878d66669f69836a563b5e2cb0
azatbek <sadykowazatbek@gmail.com>
2025-08-14 22:13:58 +0600

    feat: almost realized the stars on the rating of the institution

--- frontend/src/features/institutions/components/InstitutionCardInfo.tsx
+++ frontend/src/features/institutions/components/InstitutionCardInfo.tsx
@@ -2,6 +2,9 @@ import { useAppSelector } from '../../../app/hooks.ts';
 import { Box, Typography } from '@mui/material';
 import { selectLocation } from '../../maps/locationSlice.ts';
 import { InstitutionTypes } from '../../../types/types.Institution';
+import StarIcon from '@mui/icons-material/Star';
+import StarHalfIcon from '@mui/icons-material/StarHalf';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
 
 interface Props {
   institutions: InstitutionTypes;
@@ -12,6 +15,9 @@ const InstitutionCardInfo: React.FC<Props> = ({ institutions }) => {
 
   const phoneInfo = institutions.phoneNumber[0];
   const social = phoneInfo.socialMedia?.[0];
+
+  const ratingArrStar = Array.from({ length: institutions.rating }, (_, i) => i + 1);
+  const voidStar = Array.from({ length: 5 - ratingArrStar.length }, (_, i) => i + 1);
 
   return (
     <Box sx={{ border: "1px solid #000", p: 3, m: 2 }}>
@@ -35,7 +41,22 @@ const InstitutionCardInfo: React.FC<Props> = ({ institutions }) => {
           )}
         </div>
       </Typography>
-      {institutions.rating}
+
+      <Typography component="div" display="flex">
+        {ratingArrStar.map(() => (
+          <div>
+            <StarIcon />
+          </div>
+        ))}
+        {voidStar.map(() => (
+          <div>
+            <StarBorderIcon />
+          </div>
+        ))}
+        <div style={{ marginLeft: 5, marginTop: 2 }}>
+          {institutions.rating > 0 && institutions.rating}
+        </div>
+      </Typography>
     </Box>
   );
 };
